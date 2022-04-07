1deb856f8a765cd7a71837b2b439bc48dacb4e52
KasatkinKonstantin <kasatkin1987@yandex.ru>
2022-04-07 22:03:03 +0300

    Задание 12.7.3

---
+++ MyPython.py
@@ -0,0 +1,5 @@
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+money = float(input("Введите сумму:\n"))
+deposit = list(map(lambda summa: summa * money, list(per_cent.values())))
+print(deposit)
+print('Максимальная сумма, которую вы можете заработать - ', round(max(deposit), 2))
