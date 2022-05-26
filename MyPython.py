aa0479d08e5f9c0641fdda8605c08511cb9c7883
KasatkinKonstantin <kasatkin1987@yandex.ru>
2022-05-26 22:30:47 +0300

    add example 17.9.1

--- MyPython.py
+++ MyPython.py
@@ -1,16 +1,45 @@
-kol = int(input('Введите количество участников: '))
-sum = 0
-for i in range(kol):
-    vozr = int(input('Введите возраст ' + str(i+1) + ' участника: '))
-    if vozr >= 25:
-        print((i + 1), 'билет будет стоить 1390Р')
-        sum += 1390
-    elif 18 < vozr < 25:
-        sum += 990
-        print((i + 1), 'билет будет стоить 990Р')
+def qsort(array, left, right):
+    middle = (left + right) // 2
+    p = array[middle]
+    i, j = left, right
+    while i <= j:
+        while array[i] < p:
+            i += 1
+        while array[j] > p:
+            j -= 1
+        if i <= j:
+            array[i], array[j] = array[j], array[i]
+            i += 1
+            j -= 1
+    if j > left:
+        qsort(array, left, j)
+    if right > i:
+        qsort(array, i, right)
+
+def search(array, element, left, right):
+    if left > right:
+        return False
+    middle = (right + left) // 2
+    if (array[middle] < element) and (array[middle+1] >= element):
+        return middle
+    elif element <= array[middle]:
+        return search(array, element, left, middle - 1)
     else:
-        print((i + 1), 'билет будет бесплатным')
-if kol > 3:
-    print('Общая сумма с учетом скидки 10% равна:', sum*0.9)
+        return search(array, element, middle + 1, right)
+
+L = list(map(int, input('Введите список чисел через пробел:\n').split()))
+num = int(input('Введите число:\n'))
+position = None
+
+print('До сортировки:\n', L)
+qsort(L, 0, len(L)-1)
+print('После сортировки:\n', L)
+
+if (num <= L[0]) or (num > L[len(L)-1]):
+    print('Искомый элемент в списке не найден')
 else:
-    print('Общая сумма равна:', sum)
+    position = search(L, num, 0, len(L)-1)
+    if position:
+        print('Искомый элемент находится под номером', position+1, '\n' 'Его значение равно', L[position])
+    else:
+        print('Искомый элемент в списке не найден')
